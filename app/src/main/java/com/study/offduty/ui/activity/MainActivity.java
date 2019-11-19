fee7097bc512f41672cd7008818a7f24a0b7e191
wangjj <wangjj@autoai.com>
2019-11-19 16:30:44 +0800

    add check update function

--- app/src/main/java/com/study/offduty/ui/activity/MainActivity.java
+++ app/src/main/java/com/study/offduty/ui/activity/MainActivity.java
@@ -1,22 +1,30 @@
 package com.study.offduty.ui.activity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pgyersdk.update.DownloadFileListener;
+import com.pgyersdk.update.PgyUpdateManager;
+import com.pgyersdk.update.UpdateManagerListener;
+import com.pgyersdk.update.javabean.AppBean;
 import com.study.offduty.R;
 import com.study.offduty.ui.view.FavorLayout;
 import com.study.offduty.utils.DateUtil;
 
+import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -117,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
             }
         });
-
+        checkUpdate();
     }
 
     /**
@@ -189,5 +197,80 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         } else {
             EasyPermissions.requestPermissions(this, "请允许程序获取必要的运行权限", RC_CAMERA_AND_LOCATION, perms);
         }
+    }
+
+    /**
+     * 检查更新
+     */
+    public void checkUpdate() {
+
+        //TODO ProgressDialog过时，下载对话框替换
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setTitle("提示");
+        progressDialog.setMessage("下载中");
+        progressDialog.setMax(100);
+        progressDialog.setProgress(0);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+
+        new PgyUpdateManager.Builder()
+                .setForced(false)
+                .setUserCanRetry(true)
+                .setUpdateManagerListener(new UpdateManagerListener() {
+                    @Override
+                    public void onNoUpdateAvailable() {
+                        Log.w(TAG, "onNoUpdateAvailable");
+                    }
+
+                    @Override
+                    public void onUpdateAvailable(final AppBean appBean) {
+                        Log.w(TAG, "onUpdateAvailable");
+
+                        new AlertDialog.Builder(MainActivity.this).setIcon(R.mipmap.ic_launcher)
+                                .setTitle("提示")
+                                .setMessage("有新的更新可用，是否立即更新")
+                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        PgyUpdateManager.downLoadApk(appBean.getDownloadURL());
+                                        progressDialog.show();
+                                    }
+                                })
+                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                }).create().show();
+                    }
+
+                    @Override
+                    public void checkUpdateFailed(Exception e) {
+                        Log.w(TAG, "checkUpdateFailed");
+                        Toast.makeText(MainActivity.this, "检查更新失败", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setDownloadFileListener(new DownloadFileListener() {
+                    @Override
+                    public void downloadFailed() {
+                        Log.w(TAG, "downloadFailed");
+                    }
+
+                    @Override
+                    public void downloadSuccessful(File file) {
+                        Log.w(TAG, "downloadSuccessful");
+                        progressDialog.dismiss();
+                        PgyUpdateManager.installApk(file);
+                    }
+
+                    @Override
+                    public void onProgressUpdate(Integer... args) {
+                        Log.w(TAG, "onProgressUpdate：" + args[0]);
+                        progressDialog.setProgress(args[0]);
+
+                    }
+                })
+                .register();
     }
 }
